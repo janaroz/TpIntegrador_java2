6a1469c18d27833409962af580eb1eaa551cf476
janarozen <rozenbergjana@gmail.com>
2025-05-26 18:26:06 -0300

    Commit inicial

---
+++ TPIntegrador/src/main/java/com/alkemy/java2/TPIntegrador/TpIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.java2.TPIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegradorApplication.class, args);
+	}
+
+}
---
+++ TPIntegrador/src/test/java/com/alkemy/java2/TPIntegrador/TpIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.java2.TPIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
